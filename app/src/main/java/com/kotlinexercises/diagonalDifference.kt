531a8454fa32406aed83381e63bdf6074b103c49
Andres Bueno <andres.bueno@correounivalle.edu.co>
2024-10-21 08:12:31 -0500

    diagonalDiference

--- app/src/main/java/com/kotlinexercises/diagonalDifference.kt
+++ app/src/main/java/com/kotlinexercises/diagonalDifference.kt
@@ -3,10 +3,15 @@ package com.kotlinexercises
 import kotlin.math.abs
 
 /*
- * Complete the 'diagonalDifference' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ * Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+    For example, the square matrix  is shown below:
+        1 2 3
+        4 5 6
+        9 8 9
+    The left-to-right diagonal =1+5+9 = 15.
+    The right to left diagonal =3+5+9 = 17.
+    Their absolute difference is |15-17| = 2.
  */
 
 fun diagonalDifference(arr: Array<Array<Int>>): Int {
@@ -23,6 +28,8 @@ fun diagonalDifference(arr: Array<Array<Int>>): Int {
 
 fun main() {
     val values:Array<Array<Int>> = arrayOf(arrayOf(1,2,3), arrayOf(8,12,24),arrayOf(14,3,45))
+    val values2:Array<Array<Int>> = arrayOf(arrayOf(1,2,3), arrayOf(4,5,6),arrayOf(9,8,9))
 
     println(diagonalDifference(values)) // Diagonal1 = 1+12+45 = 58, Diagonal2 = 3+12+14=29, |58-29| = 29
+    println(diagonalDifference(values2))
 }
