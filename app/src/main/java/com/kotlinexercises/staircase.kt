a33ce342bd7219a8dc5a6f9cae6d3897e518dd94
Andres Bueno <andres.bueno@correounivalle.edu.co>
2024-10-22 10:28:27 -0500

    stairCase file

---
+++ app/src/main/java/com/kotlinexercises/staircase.kt
@@ -0,0 +1,19 @@
+package com.kotlinexercises
+/*
+    This is a staircase of size n=4:
+
+           #
+          ##
+         ###
+        ####
+    Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+    Write a program that prints a staircase of size n.
+*/
+fun main(){
+
+}
+
+
+fun staircase(n: Int): Unit {
+}
